4247113ca05b1e28f3ea96122117179cd23d4d2e
Billy Campbell <wcampbell91@gmail.com>
2020-10-17 13:31:59 -0500

    added a feed function and a __repr__ to all classes

--- cobra/cobra.py
+++ cobra/cobra.py
@@ -1,8 +1,15 @@
 from datetime import date
 
 class Cobra:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
+        self.food = food
+
+    def feed(self):
+        print(f"{self.name} was fed {self.food} at {date.today()}")
+
+    def __repr__(self):
+        print(f"{self.name} is a {self.sepecies} and was added to the zoo on {self.date_added}")
