01b64a4a0068699d2b8c4416a51b62cabebdc517
Billy Campbell <wcampbell91@gmail.com>
2020-10-24 09:46:08 -0500

    added a chip_num property and a last_animal_added property

--- attractions/PettingZoo.py
+++ attractions/PettingZoo.py
@@ -7,5 +7,9 @@ class PettingZoo:
     def add_animal(self, animal):
         self.animals.append(animal)
 
-    def __str__(self):
+    def __repr__(self):
         print(f"{self.attraction_name}: {self.description}")
+
+    @property
+    def last_animal_added(self):
+        return f"{self.animals[-1].name} was the last animal added"
